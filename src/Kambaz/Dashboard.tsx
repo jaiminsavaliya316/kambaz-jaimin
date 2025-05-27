b884f0e24252aa9e5fdd8b6d3a2227743173e178
jaiminsavaliya316 <jaiminsavaliya317@gmail.com>
2025-05-27 18:04:37 -0400

    Assignement 3 done

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,129 +1,31 @@
-import { Row, Col, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import * as db from "./Database";
+import { Row, Col, Card, Button } from "react-bootstrap";
 export default function Dashboard() {
+  const courses = db.courses;
   return (
     <div id="wd-dashboard">
- <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
- <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
- <div id="wd-dashboard-courses">
-  <Row xs={1} md={5} className="g-4">
-   <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-    <Card>
-     <Link to="/Kambaz/Courses/1234/Home"
-           className="wd-dashboard-course-link text-decoration-none text-dark">
-      <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160}/>
-      <Card.Body>
-       <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1234 React JS</Card.Title>
-       <Card.Text  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-        Full Stack software developer</Card.Text>
-       <Button variant="primary">Go</Button>
-      </Card.Body>
-     </Link>
-    </Card>
-   </Col>
-   <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
-    <Card>
-      <Link to="/Kambaz/Courses/2345/Home"
-            className="wd-dashboard-course-link text-decoration-none text-dark">
-        <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} />
-        <Card.Body>
-          <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
-            CS2345 Angular Basics
-          </Card.Title>
-          <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-            Learn the fundamentals of the Angular framework.
-          </Card.Text>
-          <Button variant="primary">Go</Button>
-        </Card.Body>
-      </Link>
-    </Card>
-   </Col>
-   <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
-    <Card>
-      <Link to="/Kambaz/Courses/3456/Home"
-            className="wd-dashboard-course-link text-decoration-none text-dark">
-        <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} />
-        <Card.Body>
-          <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
-            CS3456 Vue.js Essentials
-          </Card.Title>
-          <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-            A comprehensive guide to building apps with Vue.js.
-          </Card.Text>
-          <Button variant="primary">Go</Button>
-        </Card.Body>
-      </Link>
-    </Card>
-   </Col>
-   <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
-    <Card>
-      <Link to="/Kambaz/Courses/4567/Home"
-            className="wd-dashboard-course-link text-decoration-none text-dark">
-        <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} />
-        <Card.Body>
-          <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
-            CS4567 Node.js Fundamentals
-          </Card.Title>
-          <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-            Server-side development with Node.js and Express.
-          </Card.Text>
-          <Button variant="primary">Go</Button>
-        </Card.Body>
-      </Link>
-    </Card>
-   </Col>
-   <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
-    <Card>
-      <Link to="/Kambaz/Courses/5678/Home"
-            className="wd-dashboard-course-link text-decoration-none text-dark">
-        <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} />
-        <Card.Body>
-          <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
-            CS5678 Python Web Dev
-          </Card.Title>
-          <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-            Build web applications using Django and Flask.
-          </Card.Text>
-          <Button variant="primary">Go</Button>
-        </Card.Body>
-      </Link>
-    </Card>
-   </Col>
-   <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-    <Card>
-      <Link to="/Kambaz/Courses/6789/Home"
-            className="wd-dashboard-course-link text-decoration-none text-dark">
-        <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} />
-        <Card.Body>
-          <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
-            CS6789 Java Mastery
-          </Card.Title>
-          <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-            Deep dive into Java programming and Spring Boot.
-          </Card.Text>
-          <Button variant="primary">Go</Button>
-        </Card.Body>
-      </Link>
-    </Card>   
-   </Col>
-   <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
-    <Card>
-      <Link to="/Kambaz/Courses/7890/Home"
-            className="wd-dashboard-course-link text-decoration-none text-dark">
-        <Card.Img variant="top" src="/images/reactjs.jpg" width="100%" height={160} />
-        <Card.Body>
-          <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
-            CS7890 Data Structures
-          </Card.Title>
-          <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-            Essential data structures and algorithm techniques.
-          </Card.Text>
-          <Button variant="primary">Go</Button>
-        </Card.Body>
-      </Link>
-    </Card>   
-   </Col>
-  </Row>
-</div></div>
-
-);}
+      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+      <div id="wd-dashboard-courses">
+        <Row xs={1} md={5} className="g-4">
+          {courses.map((course) => (
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link to={`/Kambaz/Courses/${course._id}/Home`}
+                      className="wd-dashboard-course-link text-decoration-none text-dark" >
+                  <Card.Img src="/images/reactjs.jpg" variant="top" width="100%" height={160} />
+                  <Card.Body className="card-body">
+                    <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      {course.name} </Card.Title>
+                    <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                      {course.description} </Card.Text>
+                    <Button variant="primary"> Go </Button>
+                  </Card.Body>
+                </Link>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </div>);}
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,12 +1,31 @@
+// import Nav from "react-bootstrap/Nav";
+// import { NavLink } from "react-router-dom";
+// export default function TOC() {
+//  return (
+//    <Nav variant="pills">
+//     <NavLink className="nav-link" to="/Labs/Lab1">Lab 1</NavLink>
+//     <NavLink className="nav-link" to="/Labs/Lab2">Lab 2</NavLink>
+//     <NavLink className="nav-link" to="/Labs/Lab3">Lab 3</NavLink>
+//     <NavLink className="nav-link" to="/Kambaz">Kambaz</NavLink>
+//     <NavLink className="nav-link" to="https://github.com/jaiminsavaliya316/kambaz-jaimin">My GitHub</NavLink>
+//    </Nav>
+// );}
+
 import Nav from "react-bootstrap/Nav";
-import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 export default function TOC() {
- return (
-   <Nav variant="pills">
-    <NavLink className="nav-link" to="/Labs/Lab1">Lab 1</NavLink>
-    <NavLink className="nav-link" to="/Labs/Lab2">Lab 2</NavLink>
-    <NavLink className="nav-link" to="/Labs/Lab3">Lab 3</NavLink>
-    <NavLink className="nav-link" to="/Kambaz">Kambaz</NavLink>
-    <NavLink className="nav-link" to="https://github.com/jaiminsavaliya316/kambaz-jaimin">My GitHub</NavLink>
-   </Nav>
-);}
+  const { pathname } = useLocation();
+  return (
+    <Nav variant="pills" id="wd-toc">
+      <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab1" id="wd-a1"
+          active={pathname.includes("Lab1")}> Lab 1 </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab2" id="wd-a2"
+          active={pathname.includes("Lab2")}> Lab 2 </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab3" id="wd-a3"
+          active={pathname.includes("Lab3")}> Lab 3 </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link as={Link} to="/Kambaz" id="wd-a3"> Kambaz </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link href="https://github.com/jannunzi" target="_blank"> My GitHub </Nav.Link> </Nav.Item>
+    </Nav>
+  );
+}
