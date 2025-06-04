a0ce9fa6f9d7b0351f018bfc3bed3833ceaffe6e
jaiminsavaliya316 <jaiminsavaliya317@gmail.com>
2025-06-03 23:49:37 -0400

    assignment 4

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,28 @@
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
+import EventObject from "./EventObject";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
+export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div id="wd-passing-functions">
+      <h2>Lab 4</h2>
+      <ClickEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <PassingDataOnEvent />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ArrayStateVariable />
+      
+    </div>
+);}
